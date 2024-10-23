bf20dbbf8ac36e9bdf0f4e032038c1b72baa635c
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2024-10-23 18:40:58 +0600

    splitting components & building reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Player from "./components/Player";
 function App() {
-  
-
   return (
-    // <h1>React Tic-Tac-Toe</h1>
-    <p>Coming Soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
